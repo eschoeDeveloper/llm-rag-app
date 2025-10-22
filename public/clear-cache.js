9d4f76bad2c62cb139f3f63c4350bd1b42cc035a
ChoeEuiSeung <88296182+eschoeDeveloper@users.noreply.github.com>
2025-10-22 23:08:41 +0900

    Add cache-control meta tags and remove GitHub Pages script

---
+++ public/clear-cache.js
@@ -0,0 +1,11 @@
+﻿if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+caches.keys().then(function(names) {
+  for (let name of names) caches.delete(name);
+});
+location.reload(true);
